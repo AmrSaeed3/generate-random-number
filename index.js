8faa05335d29da9b1125a73c505633602633fb5e
AmrSaeed3 <amrsaid4343@gmail.com>
2024-01-04 20:50:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+(function (context, modeName, definition) {
+  //if node
+  //global
+  //module
+  //module.exports
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = definition(context);
+  }
+  //if window
+  //window obj
+  else {
+    context[modeName] = definition(context);
+  }
+})(this, "generateRandomNumber", function (context) {
+
+  function generateRandomNumber(digits) {
+    // حساب الحد الأدنى والحد الأعلى باستخدام الأسس
+    const min = Math.pow(10, digits - 1);
+    const max = Math.pow(10, digits) - 1;
+
+    // إنشاء رقم عشوائي في النطاق المحدد
+    const randomNumber = Math.floor(min + Math.random() * (max - min + 1));
+
+    // إرجاع الرقم العشوائي
+    return randomNumber;
+  }
+  return generateRandomNumber;
+});
+
